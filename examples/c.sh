f31296d0bf15f972d1d73c172b9e3aecbbbbb0ed
xb <ssbandjl@163.com>
2023-10-14 16:16:52 +0800

    uct_client

--- examples/c.sh
+++ examples/c.sh
@@ -1,3 +1,3 @@
-/home/xb/project/ucx/examples/.libs/lt-uct_hello_world -d mlx5_0:1 -t rc_verbs -n 172.17.29.63
+/home/xb/project/ucx/examples/.libs/lt-uct_hello_world -d mlx5_0:1 -t rc_verbs -n 172.17.29.63 -z
 
 
