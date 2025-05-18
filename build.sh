25b128d4e5e5884f201d21a212908eb14f65acd8
xb <bin@chinachina.cn>
2025-05-18 15:57:46 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 clear
 export HAVE_EXAMPLES=true
 ./autogen.sh
-./contrib/configure-devel --prefix=$PWD/install-debug --without-java --without-go
-make -j32
+./contrib/configure-devel --prefix=$PWD/install-debug --without-java --without-go --enable-gtest=no
+make -j4
 make install
 
 # cd examples; make
 
-search AS_HELP_STRING to enable/disable module
+# search AS_HELP_STRING to enable/disable module
