691726626a9310711249f41e016e6eee82704e24
xb <bin@chinachina.cn>
2025-05-18 15:15:39 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 clear
 export HAVE_EXAMPLES=true
 ./autogen.sh
-./contrib/configure-devel --prefix=$PWD/install-debug
-make
+./contrib/configure-devel --prefix=$PWD/install-debug --without-java --without-go
+make -j32
 make install
 
 # cd examples; make
+
+search AS_HELP_STRING to enable/disable module
